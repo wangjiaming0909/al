299f345f5cf13dfd2dfd01eee68e5b5010b1af47
Jiaming Wang <JWang284@slb.com>
2018-10-30 23:31:19 +0800

    weak_ptr: sp_convertable<son, parent>

--- cpp/test/main.cpp
+++ cpp/test/main.cpp
@@ -12,12 +12,47 @@ inline void print(const T &t) {
 	std::cout << t << std::endl;
 }
 
+void void_void(class_without_default_constructor* p) {
+	
+}
+
+void test_weak_ptr() {
+	//int* p = new int(12);
+	//void_void(p);
+	shared_ptr<class_without_default_constructor> s_ptr{ new class_without_default_constructor(1) };
+	weak_ptr<class_without_default_constructor> w_ptr{ s_ptr };
+
+	//sp_element<T[]>, although the template param of weak_ptr is class_without_default_constructor[],
+	//but the element type of it is class_without_default_constructor
+	shared_ptr<class_without_default_constructor[]> s_ptr_arr{ new class_without_default_constructor[3]{1,2,3} };//array pointer type
+	weak_ptr<class_without_default_constructor[]> w_ptr_arr{ s_ptr_arr };
+	//weak_ptr<class_without_default_constructor[]>::element_type element{ 1 };//its class_without_default_constructor type
+	assert(typeid(weak_ptr<class_without_default_constructor[]>::element_type) == typeid(class_without_default_constructor));
+
+	//at compile time, we can know that son is the drived class of parent, or compiler will complain: negative subscript
+	detail::sp_assert_convertible<share_from_me, enable_shared_from_this<share_from_me>>();
+	detail::sp_assert_convertible<son, parent>();
+	//typedef char tmp[-1];//negative subscript compile time
+
+	shared_ptr<son> sp_son{ new son() };
+	weak_ptr<son> w_ptr_son{ sp_son};
+	assert(sp_son.use_count() == 1L);
+	weak_ptr<parent> w_ptr_parent{ w_ptr_son };
+
+	assert(s_ptr.use_count() == 1L);
+	assert(w_ptr.expired() == false);
+	assert(w_ptr.owner_before(s_ptr) == false);
+}
+
+void test_enable_share_from_this() {
+	shared_ptr<share_from_me> p{ new share_from_me(1) };
+	shared_ptr<share_from_me> q = p->f();
+	assert(p == q);
+	assert(p.use_count() == 2L);
+}
+
 int main() {
-	share_from_me me{1};
-	auto ptr = me.shared_from_this();
-	//weak_ptr<share_from_me> wptr = me.weak_from_this();
-	print(*ptr);
-	print(ptr.use_count());
+	test_weak_ptr();
 }
 
 void test_shared_ptr() {
@@ -25,12 +60,12 @@ void test_shared_ptr() {
 	shared_ptr<std::_String_alloc<std::_String_base_types<char, std::allocator<char>>>> str2_ptr = str_ptr;
 	assert(str_ptr.get() == str2_ptr.get());
 	assert(!str_ptr.unique());
-	assert(str_ptr.use_count() == 2l);
+	assert(str_ptr.use_count() == 2L);
 	shared_ptr<std::string> str_ptr_swap{ new std::string("456") };
 	str_ptr.swap(str_ptr_swap);
 	assert(str_ptr.get() != str2_ptr.get());
-	assert(str2_ptr.use_count() == 2l);
-	assert(str_ptr.use_count() == 1l);
+	assert(str2_ptr.use_count() == 2L);
+	assert(str_ptr.use_count() == 1L);
 	assert(str_ptr_swap.get() == str2_ptr.get());
 	print(str_ptr);
 	print(*str_ptr);
--- cpp/test/shared_from_this_test.h
+++ cpp/test/shared_from_this_test.h
@@ -1,7 +1,15 @@
 #pragma once
 #include <boost\enable_shared_from_this.hpp>
+#include <string>
 
 using namespace boost;
+
+class class_without_default_constructor {
+public:
+	class_without_default_constructor(int a) : a_(a){}
+private:
+	int a_;
+};
 
 class share_from_me : public enable_shared_from_this<share_from_me>{
 public:
@@ -9,7 +17,21 @@ public:
 		os << me.a_;
 		return os;
 	}
-	share_from_me(int a) : a_(a) {}
+	share_from_me(int a) : a_(a), c_(a){}
+	shared_ptr<share_from_me> f() {
+		return this->shared_from_this();
+	}
 private:
 	int a_;
+	std::string str_;
+	class_without_default_constructor c_;
+};
+
+
+class parent {
+
+};
+
+class son : public parent {
+
 };
