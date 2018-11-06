bf425be6a1b1a750ed1182b0b81dbc9281cf5e83
Jiaming Wang <JWang284@slb.com>
2018-11-06 22:18:34 +0800

    002

--- cpp/test/Leetcode/main.cpp
+++ cpp/test/Leetcode/main.cpp
@@ -1,11 +1,16 @@
 
 #include <iostream>
 #include "001_two_sum.h"
+#include "002_add_two_numbers.h"
 
 using namespace std;
 
 int main() {
-	two_sum_test();
+	//two_sum_test();
+	add_two_numbers_test2();
+	add_two_numbers_test3();
+	//list_node2long_test();
+	//char_2_list_node_test();
 
 	cout << 1 << endl;
 }
