ab950df6c5cb161af230184eff85f44ef56bf608
Jiaming Wang <JWang284@slb.com>
2018-11-12 22:32:53 +0800

    003

--- cpp/test/Leetcode/003_longest_substring_without_repeating_characters.h
+++ cpp/test/Leetcode/003_longest_substring_without_repeating_characters.h
@@ -3,9 +3,37 @@
 #include <cstdio>
 #include <vector>
 #include <string>
+#include <unordered_set>
+#include <cassert>
 
 using std::string;
 using std::vector;
+using std::unordered_set;
 
-vector<string> split_string(string s, char c) {
+int lengthOfLongestSubstring(string s) {
+	int len = s.size();
+	int max_len = 0;
+	unordered_set<char> str_set{};
+	int start = 0, end = 0;
+	for (int i = start; i < len; i++) {
+		for (int j = i; j < len; j++) {
+			if (str_set.count(s.at(j)) == 0) {
+				str_set.insert(s.at(j));
+				end = j+1;
+				continue;
+			} 
+			max_len = std::max(max_len, j - i);
+			start++;
+			break;
+		}
+		max_len = std::max(max_len, end - i);
+		str_set.clear();
+	}
+	return max_len;
+}
+
+void test_lengthOfLongestSubstring() {
+	string s = "w";
+	int ret = lengthOfLongestSubstring(s);
+	assert(ret == 1);
 }
--- cpp/test/Leetcode/main.cpp
+++ cpp/test/Leetcode/main.cpp
@@ -2,15 +2,17 @@
 #include <iostream>
 #include "001_two_sum.h"
 #include "002_add_two_numbers.h"
+#include "003_longest_substring_without_repeating_characters.h"
 
 using namespace std;
 
 int main() {
 	//two_sum_test();
-	add_two_numbers_test2();
-	add_two_numbers_test3();
+	//add_two_numbers_test2();
+	//add_two_numbers_test3();
 	//list_node2long_test();
 	//char_2_list_node_test();
+	test_lengthOfLongestSubstring();
 
 	cout << 1 << endl;
 }
