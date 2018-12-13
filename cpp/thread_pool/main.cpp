98ef68710d964368d3948699443cfecde31148ad
Jiaming Wang <JWang284@slb.com>
2018-12-13 16:38:08 +0800

    123

--- cpp/thread_pool/main.cpp
+++ cpp/thread_pool/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-	cout << thread::hardware_concurrency() << endl;
+	cout << thread::hardware_concurrency() << endl;//8
 	cout << 123 << endl;
 	char a[8];
 	cin >> a;
