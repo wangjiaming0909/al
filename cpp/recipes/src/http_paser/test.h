129e59b3428ffcfe27af56ab7a36392b6f760768
=wangjiaming0909 <=>
2019-05-24 22:53:40 +0800

    123

--- cpp/recipes/src/http_paser/test.h
+++ cpp/recipes/src/http_paser/test.h
@@ -1,7 +1,6 @@
 #ifndef _HTTP_PASER_TEST_H_
 #define _HTTP_PASER_TEST_H_
 
-#ifdef __gnu_linux__
 
 #include <iostream>
 #include "http_paser/http_paser.h"
@@ -12,14 +11,15 @@ namespace http_paser_test
 
 void test()
 {
+#ifdef __gnu_linux__
     http_parser_url url;
     const char *site = "http://www.github.com/wangjiaming0909";
     http_parser_parse_url(site, strlen(site), 0, &url);
+#endif //__gnu_linux__
     cout << 123 << endl;
 }
 
 }
 
-#endif //linux
 
 #endif // _HTTP_PASER_TEST_H_
