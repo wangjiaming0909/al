b12a930948391b9907a40ee80aec06eee17131d5
wangjiaming0909 <604227650@qq.com>
2019-05-27 21:00:48 +0800

    string_piece

---
+++ cpp/recipes/src/string_piece/string_piece.cpp
@@ -0,0 +1,24 @@
+#include "string_piece/string_piece.h"
+
+namespace string_piece
+{
+
+string_piece::string_piece(const char* str)
+{
+}
+
+string_piece::~string_piece()
+{
+}
+
+string_piece::string_piece(const string_piece& other)
+{
+
+}
+
+string_piece& string_piece::operator=(const string_piece& other)
+{
+
+}
+
+}
--- cpp/recipes/src/string_piece/string_piece.h
+++ cpp/recipes/src/string_piece/string_piece.h
@@ -15,23 +15,6 @@ private:
     // const char*
 };
 
-string_piece::string_piece(const char* str)
-{
-}
-
-string_piece::~string_piece()
-{
-}
-
-string_piece::string_piece(const string_piece& other)
-{
-
-}
-
-string_piece& string_piece::operator=(const string_piece& other)
-{
-
-}
 }
 #endif //_STRING_PIECE_H_
 
