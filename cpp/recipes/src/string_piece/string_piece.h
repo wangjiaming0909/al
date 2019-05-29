a373bd58d7eabad970d1b76d68843ae2791bb11c
wangjiaming0909 <604227650@qq.com>
2019-05-29 23:34:52 +0800

    string piece

--- cpp/recipes/src/string_piece/string_piece.h
+++ cpp/recipes/src/string_piece/string_piece.h
@@ -5,9 +5,24 @@
 #include <cstring>
 #include <algorithm>
 #include <stdexcept>
+#include <ostream>
 
 namespace string_piece
 {
+template <class T>
+struct IsCharPointer {};
+
+template <>
+struct IsCharPointer<char*> {
+  typedef int type;
+};
+
+template <>
+struct IsCharPointer<const char*> {
+  typedef int const_type;
+  typedef int type;
+};
+
 template <typename Iter>
 class Range
 {
@@ -27,8 +42,9 @@ public:
     Range(iterator begin, iterator end) : begin_(begin), end_(end){}
     Range(iterator begin, size_type size) : begin_(begin), end_(begin + size){}
 
-//应该只在Iter时包含const时才能调用此构造函数
-    // Range(const std::string& str) : begin_(str.data()), end_(str.data() + str.size()) {}
+    //应该只在Iter时包含const时才能调用此构造函数
+    template <typename T = Iter, typename IsCharPointer<T>::const_type = 0>
+    Range(const std::string &str) : begin_(str.data()), end_(str.data() + str.size()) {}
     Range(iterator begin) : begin_(begin), end_(begin_ + ::strlen(begin_)){}
 
     Range(const Range&) = default;
@@ -46,6 +62,11 @@ public:
         return it == end_ ? std::string::npos : (it - begin_);
     }
 
+    size_type find(Range<Iter> str)
+    {
+
+    }
+
     Range<iterator> sub_string(size_type first, size_type len = std::string::npos)
     {
         if(first > size())
@@ -59,10 +80,19 @@ public:
     {
         return size_type(end_ - begin_);
     }
+    template <typename T>
+    friend std::ostream &operator<<(std::ostream &os, Range<T> str);
+
 private:
     iterator begin_;
     iterator end_;
 };
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, Range<T> str)
+{
+    return os << str.begin_;
+}
 
 using mutable_string_piece = Range<char*>;
 using const_string_piece = Range<const char*>;
--- cpp/recipes/src/string_piece/test.h
+++ cpp/recipes/src/string_piece/test.h
@@ -4,10 +4,42 @@
 #include "string_piece/string_piece.h"
 #include <iostream>
 #include <vector>
+#include <cassert>
 using namespace std;
 
 namespace string_piece_test
 {
+
+void test_sub_string()
+{
+    std::string str = "1234567890";
+    string_piece::const_string_piece sp{str};
+
+    auto subString = sp.sub_string(0, 10);
+    assert(subString.begin() == &*str.begin());
+    assert(subString.end() == &*str.end());
+    assert(subString.size() == str.length());
+}
+
+void test_sub_string2()
+{
+    const char* data = "123456789";
+    char* ptr = static_cast<char*>(::calloc(10, 1));
+    ::memcpy(ptr, data, 6);
+    string_piece::mutable_string_piece str2{ptr, ptr + 5};
+
+    auto subString = str2.sub_string(0, 5);
+    assert(subString.begin() == ptr);
+    assert(subString.end() == ptr + 5);
+
+    ::free(ptr);
+}
+
+void test_find()
+{
+
+}
+
 
 void test()
 {
@@ -23,19 +55,21 @@ void test()
     cout << "index: " << index << endl;
 
     std::string strstr = "123456";
+    //mutable_string_piece do not have this constructor
     // string_piece::mutable_string_piece str3{strstr};
 
-    // string_piece::const_string_piece str4{strstr};
+    string_piece::const_string_piece str4{strstr};
     string_piece::mutable_string_piece str5{ptr};
 
-
-
-
+    ::free(ptr);
 
     // std::vector<int> v{1,2,3};
     // string_piece::Range<int *> int1{&*v.begin()};
 
     cout << 123 << endl;
+
+    test_sub_string();
+    test_sub_string2();
 }
 
 }
