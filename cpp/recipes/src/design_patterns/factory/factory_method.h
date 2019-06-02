2f6763466521ae9eb192759a124ee3bb0da9ac43
wangjiaming0909 <604227650@qq.com>
2019-06-02 16:56:16 +0800

    factory method

--- cpp/recipes/src/design_patterns/factory/factory_method.h
+++ cpp/recipes/src/design_patterns/factory/factory_method.h
@@ -4,6 +4,15 @@
 namespace design_patterns
 {
 
+/*motivation: 
+如果 不使用static 方法来提供创建对象的方法，
+那么可能是直接构造子类(SelectReactorImpl 或者 PollReactorImpl)
+但是可能只是想让客户知道ReactorImpl这个基类,而select和poll是具体实现，并不像暴露
+
+或者ReactorImpl 可以提供一个enum 作为构造函数的参数, 来构造特定类型的ReactorImpl,
+但是如果特定类型较多，那么在基类的构造函数中可能会看到一个大的switch case
+*/
+
 class ReactorImpl{
 protected:
     ReactorImpl(int handle) : handle_(handle){}
