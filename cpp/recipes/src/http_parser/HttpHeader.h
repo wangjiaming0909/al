8d30576224a1a575ebe8c38e8426f23a3d9aff8c
wangjiaming0909 <604227650@qq.com>
2019-06-14 23:17:19 +0800

    Http header and http method

---
+++ cpp/recipes/src/http_parser/HttpHeader.h
@@ -0,0 +1,25 @@
+#ifndef _HTTP_HEADER_H_
+#define _HTTP_HEADER_H_
+
+namespace http
+{
+class HttpHeader
+{
+private:
+    /* data */
+public:
+    HttpHeader(/* args */);
+    ~HttpHeader();
+};
+
+HttpHeader::HttpHeader(/* args */)
+{
+}
+
+HttpHeader::~HttpHeader()
+{
+}
+
+
+}
+#endif // _HTTP_HEADER_H_
---
+++ cpp/recipes/src/http_parser/HttpMethod.h
@@ -0,0 +1,26 @@
+#ifndef _HTTP_METHOD_H_
+#define _HTTP_METHOD_H_
+
+namespace http
+{
+
+class HttpMethod
+{
+private:
+    /* data */
+public:
+    HttpMethod(/* args */);
+    ~HttpMethod();
+};
+
+HttpMethod::HttpMethod(/* args */)
+{
+}
+
+HttpMethod::~HttpMethod()
+{
+}
+
+
+}
+#endif // _HTTP_METHOD_H_
