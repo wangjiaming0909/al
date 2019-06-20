e6d5f04036303facf2769879db13bc6e6d74e068
wangjiaming0909 <604227650@qq.com>
2019-06-20 13:38:04 +0800

    http methods string to method

--- cpp/recipes/src/http/HttpMethod.cpp
+++ cpp/recipes/src/http/HttpMethod.cpp
@@ -1,6 +1,32 @@
 #include "http/HttpMethod.h"
+#include <string>
+#include <vector>
 
 namespace http
 {
 
+#define HTTP_METHOD_STRING(method) #method
+
+const std::vector<std::string> getHttpMethodString()
+{
+    static const std::vector<std::string>* httpMethodStrings = 
+    new std::vector<std::string>
+    {
+        HTTP_METHOD_GEN(HTTP_METHOD_STRING)
+    };
+    return *httpMethodStrings;
+}
+
+boost::optional<HTTPMethod> stringToMethod(string_piece::const_string_piece methodName)
+{
+    const auto& methodStrings = getHttpMethodString();
+    for(int i = 0; i < methodStrings.size(); i++)
+    {
+        if(methodName.caseInsensitiveEqual(methodStrings[i].c_str()))
+        {
+            return HTTPMethod(i);
+        }
+    }
+    return boost::none;
+}
 }
--- cpp/recipes/src/http/HttpMethod.h
+++ cpp/recipes/src/http/HttpMethod.h
@@ -1,27 +1,32 @@
 #ifndef _HTTP_METHOD_H_
 #define _HTTP_METHOD_H_
+#include "string_piece/string_piece.h"
+#include "boost/optional.hpp"
 
 namespace http
 {
+#define HTTP_METHOD_GEN(x) \
+  x(GET),                  \
+  x(POST),                 \
+  x(OPTIONS),              \
+  x(DELETE),               \
+  x(HEAD),                 \
+  x(CONNECT),              \
+  x(PUT),                  \
+  x(TRACE),                \
+  x(PATCH),                \
+  x(SUB),                  \
+  x(PUB),                  \
+  x(UNSUB)
+
+#define HTTP_METHOD_ENUM(method) method
 
 enum class HTTPMethod
 {
-    GET,
-    POST,
-    OPTIONS,
-    DELETE,
-    HEAD,
-    CONNECT,
-    PUT,
-    TRACE,
-    PATCH,
-    SUB,
-    PUB,
-    UNSUB
+    HTTP_METHOD_GEN(HTTP_METHOD_ENUM)
 };
 
-
-
+boost::optional<HTTPMethod> stringToMethod(string_piece::const_string_piece methodName);
 
 }
 #endif // _HTTP_METHOD_H_
