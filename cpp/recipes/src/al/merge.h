5195f02aeb041cea57b3f4a402ade7838062247f
wangjiaming0909 <604227650@qq.com>
2019-07-04 11:05:48 +0800

    merge time test

--- cpp/recipes/src/al/merge.h
+++ cpp/recipes/src/al/merge.h
@@ -77,34 +77,46 @@ void test_mergeMultiSortedArray()
     std::vector<double> v1 = {1,2,3,4,5,6,7, 8, 9, 10, 11, 12 , 13, 15};
     std::vector<double> v2 = {1,2,8,9,15,16,17, 19, 21, 22, 23, 24, 25, 27};
     std::vector<double> v3 = {1,2,3,4,15,26,27, 31, 99, 120, 222, 333, 444, 555, 666, 777, 8888, 100000};
+    std::vector<double> v4{};
+    std::vector<double> v5{};
+    std::vector<double> v6{};
+    for(int i = 0; i < 999999; i++)
+    {
+        v4.push_back((i + 0.1) * 2);
+        v5.push_back((i + 0.5) * 2.4);
+        v6.push_back((i * 1.2) + 2.5 );
+    }
 
     std::vector<std::vector<double>> arrays{};
 
     arrays.push_back(v1);
     arrays.push_back(v2);
     arrays.push_back(v3);
+    arrays.push_back(v4);
+    arrays.push_back(v5);
+    arrays.push_back(v6);
 
     std::vector<double> ret{};
 
     {
-        utils::timer _{"mergeMultiSortedArray1"};
+        utils::timer _{"mergeMultiSortedArray1"};//1.0551276s
         ret = mergeMultiSortedArray1(arrays);
     }
 
-    for(size_t i = 0; i < ret.size(); i++)
-    {
-        std::cout << "index: " << i << " " << ret[i] << std::endl;
-    }
+    // for(size_t i = 0; i < ret.size(); i++)
+    // {
+        // std::cout << "index: " << i << " " << ret[i] << std::endl;
+    // }
 
     {
-        utils::timer _{"mergeMultiSortedArrays with minheap"};
+        utils::timer _{"mergeMultiSortedArrays with minheap"};//1.069112s
         ret = mergeMultiSortedArray2(arrays);
     }
 
-    for(size_t i = 0; i < ret.size(); i++)
-    {
-        std::cout << "index: " << i << " " << ret[i] << std::endl;
-    }
+    // for(size_t i = 0; i < ret.size(); i++)
+    // {
+        // std::cout << "index: " << i << " " << ret[i] << std::endl;
+    // }
 
 }
 
