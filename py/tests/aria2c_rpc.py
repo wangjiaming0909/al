acd8e467654869c91a86b6cb820561fe0f82e439
wangjiaming0909 <604227650@qq.com>
2019-12-17 23:54:20 +0800

    aria2c rpc

---
+++ py/tests/aria2c_rpc.py
@@ -0,0 +1,59 @@
+from urllib import request
+import json
+import sys
+import time
+
+if len(sys.argv) < 2:
+    print('argv error')
+    exit(-1)
+url = sys.argv[1]
+
+
+def getUrl(id, method, params):
+    return json.dumps({'jsonrpc': '2.0',
+                       'id': id,
+                       'method': method,
+                       'params': [[params]]}).encode()
+
+
+def getReq(id, method, params):
+    return json.dumps({'jsonrpc': '2.0',
+                       'id': id,
+                       'method': method,
+                       'params': [params]}).encode()
+
+
+id = 'jiaming'
+jsonReq = getUrl(id, 'aria2.addUri', url)
+print(jsonReq)
+
+rpcUrl = 'http://localhost:6800/jsonrpc'
+
+res = request.urlopen(rpcUrl, jsonReq)
+
+if res.status != 200:
+    print(res.status)
+    exit(-1)
+data = res.read()
+data = json.loads(data)
+result = str(data['result'])
+print(data)
+print(result)
+
+for i in range(0, 100):
+    time.sleep(2)
+
+    jsonReq = getReq(id, 'aria2.tellStatus', result)
+
+    print(jsonReq)
+    res = request.urlopen(rpcUrl, jsonReq)
+
+    if res.status != 200:
+        print(res.data)
+        exit(-1)
+    j = json.loads(res.read())
+    print(j['result']['downloadSpeed'])
+    completed = j['result']['files']
+    total = j['result']['totalLength']
+    print(completed)
+    print(total)
--- py/tests/test_Eof.py
+++
@@ -1,8 +0,0 @@
-import os.path
-
-
-fileNameToWrite = 'tests/eof.tmp'
-
-toWritePyFile = open(fileNameToWrite, 'a')
-toWritePyFile.write('123\n123\n123\n')
-toWritePyFile.flush()
