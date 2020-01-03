9d639c78f4eef8d7e5d8781f2bd3c32d12f716c7
Jiaming Wang <jwang284@slb.com>
2020-01-03 14:22:19 +0800

    setup on windows

--- cpp/windowscpp/windowscpp/Source.cpp
+++ cpp/windowscpp/windowscpp/Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "sockpp/connector.h"
 #include "sockpp/inet_address.h"
+#include "proto/mess_wl.pb.h"
 using namespace std;
 
 void init()
@@ -28,5 +29,16 @@ int main()
 	std::string name = "ubuntu";
 	sockpp::inet_address target = sockpp::inet_address(name, 80);
 	auto connector = sockpp::connector(target);
+	downloadmessage::Mess_WL mess{};
+	mess.set_id(1);
+	mess.set_command(downloadmessage::Mess_WL::DownloadCommand::Mess_WL_DownloadCommand_DOWNLOAD);
+	mess.set_url("");
+	mess.set_len(mess.ByteSizeLong());
+	cout << mess.ByteSizeLong() << endl;
+	cout << 
+		mess.url().size() + 
+		sizeof(downloadmessage::Mess_WL::DownloadCommand) + 
+		sizeof(mess.id()) + 
+		sizeof(mess.len()) << endl;
 	return 0;
 }
