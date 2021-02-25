b55d0712c0de586d820e5ae2658803ed153c26f6
wangjiaming0909 <wangjiaming0909@sina.com>
2021-02-25 22:33:17 +0800

    curses

--- py/tests/main.py
+++ py/tests/main.py
@@ -1,7 +1,39 @@
-import xalpha as xa
-import matplotlib
+#!/bin/python3
 
-if __name__ == '__main__':
-    f = xa.fundinfo('519736')
-    p = f.price.plot(x='date', y=['netvalue'])
-    print(f)
+
+import curses
+import time
+
+class Ele:
+    def __init__(self, y:int, x:int, content:str):
+        self.x = x
+        self.y = y
+        self.content = content
+        self.attr = None
+
+def refresh_win(w, eles: list):
+    for e in eles:
+        if e.attr:
+            w.addstr(e.y, e.x, e.content, attr=e.attr)
+        else:
+            w.addstr(e.y, e.x, e.content)
+
+stdw = curses.initscr()
+curses.cbreak()
+curses.raw()
+e1 = Ele(1, 0, "100100100100\t100100100100\n")
+e2 = Ele(2, 0, "200200200200\t200200200\n")
+eles = [e1,e2]
+try:
+    c = '1'
+    while c != 'q':
+        line = "cols: " + str(curses.COLS) + " rows: " + str(curses.LINES)
+        ele_col_line = Ele(0,0,line)
+        eles.append(ele_col_line)
+        refresh_win(stdw, eles)
+        c = stdw.getch()
+except Exception as e:
+    print(str(e.args))
+finally:
+    curses.endwin()
+
